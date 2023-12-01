19c5d9cac57fe0cbcd3e75b83d876e535fe7080a
Gabriel de Mendonça <gb.mendonca21@gmail.com>
2023-12-01 17:55:43 -0300

    feat(tela3): produção da tela 3

---
+++ tela3.js
@@ -0,0 +1,10 @@
+document.getElementById("formulario").addEventListener("submit", function(event){
+    event.preventDefault()
+    window.location.href="pag4.html"
+    
+})
+
+document.getElementById("formulario").addEventListener("change", function(event){
+    var selectedImg = document.getElementById()
+
+})
