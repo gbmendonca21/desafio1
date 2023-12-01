3bdd4a03a740d06334bebca199f72ec0801d1d32
Gabriel de Mendonça <gb.mendonca21@gmail.com>
2023-12-01 17:56:09 -0300

    feat(tela4): produção da tela 4

---
+++ tela4.js
@@ -0,0 +1,10 @@
+document.getElementById("formulario").addEventListener("submit", function(event){
+    event.preventDefault()
+    window.location.href="pag5.html"
+    
+})
+
+document.getElementById("formulario").addEventListener("change", function(event){
+    var selectedImg = document.getElementById()
+
+})
